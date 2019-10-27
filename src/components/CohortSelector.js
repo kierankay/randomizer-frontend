e594a1659c0a00cab532ce79cacb65c238b02566
Kieran Kay <kierankay@gmail.com>
2019-10-26 18:22:00 -0700

    Update fixes delayed selection propagation

--- src/components/CohortSelector.js
+++ src/components/CohortSelector.js
@@ -10,31 +10,34 @@ class CohortSelector extends React.Component {
   }
   // Review if the chain of data loading isn't doing something weird
   async componentDidMount() {
-    await this.props.getCohortsFromApi(); 
+    await this.props.getCohortsFromApi();
     if (this.props.currentCohort) {// loads cohorts from server into redux store
-      this.setState({cohort: this.props.currentCohort})
+      this.setState({ cohort: this.props.currentCohort })
     } // add first cohort from server to redux store
   }
 
   async handleChange(evt) {
-    this.setState({ [evt.target.name]: evt.target.value }); // add cohort to state
-    await this.props.updateCurrentCohort(this.state.cohort); // update currentCohort to redux
-    if (this.props.loadIntoCohort) {
-      this.props.loadIntoCohort(this.state.cohort)
-    }
+    this.setState({ [evt.target.name]: evt.target.value },
+      async () => {
+        await this.props.updateCurrentCohort(this.state.cohort); // update currentCohort to redux
+        if (this.props.loadIntoCohort) {
+          this.props.loadIntoCohort(this.state.cohort)
+        }
+      }); // add cohort to state
   }
 
   render() {
-    let cohortFields = this.props.cohorts ? this.props.cohorts.map(c => <option value={c.cohort_name}>{c.cohort_name}</option>) : "Loading"
+    let cohortFields = this.props.cohorts ? this.props.cohorts.map(c => 
+        <option key={c.cohort_name} value={c.cohort_name}>{c.cohort_name}</option>) : "Loading"
     return (
-        <form onChange={this.handleChange}>
-          <div className="form-group">
-            <label htmlFor="cohort">Select a cohort</label>
-            <select className="form-control" id="cohort" name="cohort">
-              {cohortFields}
-            </select>
-          </div>
-        </form>
+      <form onChange={this.handleChange}>
+        <div className="form-group">
+          <label htmlFor="cohort">Select a cohort</label>
+          <select className="form-control" id="cohort" name="cohort" value={this.state.cohort}>
+            {cohortFields}
+          </select>
+        </div>
+      </form>
     )
   }
 }
