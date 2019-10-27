c385dab1003ad482acf55459bf0219163d41ec16
Kieran Kay <kierankay@gmail.com>
2019-10-27 01:01:46 -0700

    move app and server

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ExpressError = require('./expressError');
+const ExpressError = require('./src/expressError');
 const app = express();
 
 app.use('/', express.static('./'));
