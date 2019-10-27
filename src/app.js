4f9f67e389da2337e4af4bfd2084b2af8bca81e0
Kieran Kay <kierankay@gmail.com>
2019-10-27 00:33:39 -0700

    add expressError and update static path

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const fs = require('fs')
-
+const fs = require('fs');
 const app = express();
 
-app.use('/', express.static('./'))
+app.use('/', express.static('./'));
 
 app.get('/', async function (req, res, next) {
   try {
