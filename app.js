468a911abf0087cb83ccf4d8873557946758f2c3
Kieran Kay <kierankay@gmail.com>
2019-11-12 12:14:42 -0800

    rewrite static URL folder to always serve from root

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ExpressError = require('./src/expressError');
 const app = express();
 
-app.use('/', express.static('./'));
+app.use('/', express.static(__dirname));
 
 app.get('*', async function (req, res, next) {
   try {
