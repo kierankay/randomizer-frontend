96e9a811b78adda3fe085157e34b3d23ecc3ea5a
Kieran Kay <kierankay@gmail.com>
2019-11-11 23:22:33 -0800

    update Routes with new Forgot Password and Reset Password routes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import GreetingPage from './GreetingPage';
+import ForgotPasswordForm from '../components/screens/ForgotPasswordForm';
+import ResetPasswordForm from '../components/screens/ResetPasswordForm';
 import LoginFormContainer from '../containers/LoginFormContainer';
 import AdminConsoleContainer from '../containers/AdminConsoleContainer';
 import GroupsListContainer from '../containers/GroupsListContainer';
 import SignUpFormContainer from '../containers/SignUpFormContainer';
-import GreetingPage from './GreetingPage';
 
 class Routes extends React.Component {
   render() {
@@ -21,6 +23,8 @@ class Routes extends React.Component {
             null
           }
           <Route exact path='/sign-up' render={(rtProps) => <SignUpFormContainer {...rtProps} />} />
+          <Route exact path='/forgot-password' render={(rtProps) => <ForgotPasswordForm {...rtProps} />} />
+          <Route exact path='/reset-password/:token' render={(rtProps) => <ResetPasswordForm {...rtProps} />} />
           <Route path='/' render={(rtProps) => <GreetingPage {...rtProps} />} />
         </Switch>
       </React.Fragment>
