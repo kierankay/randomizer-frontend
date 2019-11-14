a062ca1bcb0acc3efaeeab21a7a570ec6343a592
Kieran Kay <kierankay@gmail.com>
2019-11-13 23:51:27 -0800

    Fix Cohort.js bug where no selected cohort results in an error

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -16,12 +16,13 @@ const Cohort = (props) => {
     addStudentToApi(firstName, lastName, cohort);
     setFirstName('');
     setLastName('');
+    setAddingStudent(false);
   }
 
   return (
     <div className="card card-header mb-3" style={{ backgroundColor: "rgb(218, 248, 226)" }}>
       <h1 style={{ display: "inline" }}>{cohort} <span style={{ fontSize: "20px" }}></span></h1>
-      {students.map(s => <Student key={s.first_name + s.last_name} firstName={s.first_name} lastName={s.last_name} />)}
+      {students ? students.map(s => <Student key={s.first_name + s.last_name} firstName={s.first_name} lastName={s.last_name} />) : null}
       {addingStudent ?
         <form onSubmit={handleSubmit}>
           <div className="form-group">
@@ -30,7 +31,7 @@ const Cohort = (props) => {
           </div>
           <div className="form-group">
             <label htmlFor="lastName">Last Name</label>
-            <input type="text" className="form-control" name="lastName" id="lastName" aria-describedby="helpId" value={lastName} onChange={(evt) => setLastName(evt.target.value)}  />
+            <input type="text" className="form-control" name="lastName" id="lastName" aria-describedby="helpId" value={lastName} onChange={(evt) => setLastName(evt.target.value)} />
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
