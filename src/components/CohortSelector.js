60e69b499cf4b6a68e4e4fe71b1f61a8ea996356
Kieran Kay <kierankay@gmail.com>
2019-11-14 17:30:27 -0800

    update CohortSelector.js to use cohort object and cohort ID

--- src/components/CohortSelector.js
+++ src/components/CohortSelector.js
@@ -7,30 +7,30 @@ const CohortSelector = (props) => {
   useEffect(() => {
     async function fetchData() {
       await getCohortsFromApi(); // loads cohorts from server into redux store
-      setCohort(currentCohort) // add first cohort from server to redux store
+      setCohort(currentCohort) // add any current cohort from redux store to form
     }
     fetchData();
   }, []);
 
   useEffect(() => {
     async function updateData() {
-      await updateCurrentCohort(cohort); // update currentCohort to store
+      await updateCurrentCohort(cohort); // update selected cohort to store
       if (loadIntoCohort && cohort) { // add cohort to state
         loadIntoCohort(cohort);
       }
 
-      await getStudentsFromApi(cohort); // add students to store
+      await getStudentsFromApi(cohort.id); // add students to store
     }
     updateData();
   }, [cohort]);
 
   let cohortFields = cohorts ? cohorts.map(c =>
-    <option key={c.cohort_name} value={c.cohort_name}>{c.cohort_name}</option>) : "Loading";
+    <option key={c.id} value={JSON.stringify(c)}>{c.cohort_name}</option>) : "Loading";
   return (
     <form>
       <div className="form-group">
         <label htmlFor="cohort">Select a cohort</label>
-        <select className="form-control" id="cohort" name="cohort" value={cohort} onChange={(evt) => setCohort(evt.target.value)}>
+        <select className="form-control" id="cohort" name="cohort" onChange={(evt) => setCohort(JSON.parse(evt.target.value))}>
           <option></option>
           {cohortFields}
         </select>
