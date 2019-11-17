3b69adaf45cb6e457a310cd192b9d959e5f5348d
Kieran Kay <kierankay@gmail.com>
2019-11-16 16:21:57 -0800

    Encapsulate pairs in padding to have it match padding in the rest of a the Cohort card

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Pair from './Pair';
-import NewGroupSaveForm from './NewGroupSaveForm'
+import NewGroupSaveForm from './NewGroupSaveForm';
 
 const Group = props => {
   let { project, date, pairs, type, handleSave } = props;
 
   return (
-    <div className="card card-header mb-3" style={type ? { backgroundColor: "rgb(218, 248, 226)" } : null}>
+    <div className="card card-header my-4" style={type ? { backgroundColor: "rgb(218, 248, 226)" } : null}>
       {project ?
         <h1 style={{ display: "inline" }}>{project} <span style={{ fontSize: "20px" }}>{date}</span></h1> :
         <NewGroupSaveForm handleSave={handleSave} />
       }
-      {pairs.map((pair, idx) => <Pair key={project + date + idx} students={pair} pairIdx={idx} />)}
+      <div className="px-3 my-3">{pairs.map((pair, idx) => <Pair key={project + date + idx} students={pair} pairIdx={idx} />)}</div>
     </div>
   )
 }
