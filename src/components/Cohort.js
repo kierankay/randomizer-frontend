fb9ec4bcc31845274f6c2ba166e188350fb5c136
Kieran Kay <kierankay@gmail.com>
2019-11-16 16:18:24 -0800

    Update Cohort component styling
    - Use flexbox for student component arrangement
    - Switch from inline style to bootstrap classes for col widths
    - Update padding and student component sizes

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -21,22 +21,28 @@ const Cohort = (props) => {
   }
 
   return (
-    <div className="card card-header mb-3" style={{ backgroundColor: "rgb(218, 248, 226)" }}>
-      <h1 style={{ display: "inline" }}>{cohort.cohort_name} <span style={{ fontSize: "20px" }}></span></h1>
-      {students ? students.map(s => <Student key={s.first_name + s.last_name} firstName={s.first_name} lastName={s.last_name} />) : null}
-      {addingStudent ?
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" className="form-control" name="firstName" id="firstName" aria-describedby="helpId" value={firstName} onChange={(evt) => setFirstName(evt.target.value)} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" className="form-control" name="lastName" id="lastName" aria-describedby="helpId" value={lastName} onChange={(evt) => setLastName(evt.target.value)} />
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-        : <button className="btn btn-primary" onClick={() => setAddingStudent(!addingStudent)}>Add Student</button>}
+    <div className="card card-header my-3" style={{ backgroundColor: "rgb(218, 248, 226)" }}>
+      <div className="row d-flex flex-row p-3">
+        <h1 className="d-block col-12">{cohort.cohort_name} <span style={{ fontSize: "20px" }}></span></h1>
+        {students ? students.map(s => <Student key={s.first_name + s.last_name} firstName={s.first_name} lastName={s.last_name}/>) : null}
+      </div>
+      <div>
+        {addingStudent ?
+          <form onSubmit={handleSubmit} className="row d-flex flex-row">
+            <div className="form-group col-6">
+              <label htmlFor="firstName">First Name</label>
+              <input type="text" className="form-control" name="firstName" id="firstName" aria-describedby="helpId" value={firstName} onChange={(evt) => setFirstName(evt.target.value)} />
+            </div>
+            <div className="form-group col-6">
+              <label htmlFor="lastName">Last Name</label>
+              <input type="text" className="form-control" name="lastName" id="lastName" aria-describedby="helpId" value={lastName} onChange={(evt) => setLastName(evt.target.value)} />
+            </div>
+            <div className="col-12">
+            <button type="submit" className="btn btn-primary">Submit</button>
+            </div>
+          </form>
+          : <div className="d-block"><button className="btn btn-primary mt-3" onClick={() => setAddingStudent(!addingStudent)}>Add Student</button></div>}
+      </div>
     </div>
   )
 }
