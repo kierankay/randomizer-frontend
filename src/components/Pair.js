d6e9dbffe0f3cfaa1924ada5dea68c439c25f4a4
Kieran Kay <kierankay@gmail.com>
2019-11-16 16:20:42 -0800

    Update pair display
    Pairs now have an encapsulating div with .col-10 that allows us to use .col-6 on a student and still have it fit on one row with the pair ID

--- src/components/Pair.js
+++ src/components/Pair.js
@@ -3,15 +3,17 @@ import Student from './Student';
 
 const Pair = props => {
   const { student_1, student_2 } = props.students;
-  const { pairIdx } = props
-  
+  const { pairIdx } = props;
+
   return (
     <div>
       {
         <div className="row">
           <div className="card col-2"><div className="card-body">{pairIdx}</div></div>
-          {student_1 ? <Student firstName={student_1.first_name} lastName={student_1.last_name} /> : null}
-          {student_2 ? <Student firstName={student_2.first_name} lastName={student_2.last_name} /> : null}
+          <div className="col-10 d-flex">
+            {student_1 ? <Student firstName={student_1.first_name} lastName={student_1.last_name} /> : null}
+            {student_2 ? <Student firstName={student_2.first_name} lastName={student_2.last_name} /> : null}
+          </div>
         </div>
       }
     </div>
