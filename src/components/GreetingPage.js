3c21f4f3b7165ef985e3c6bb76550475d6915770
kierankay <kierankay@gmail.com>
2020-02-01 15:39:06 -0800

    update GreetingPage - add more descriptive introduction to teach users why this is important

--- src/components/GreetingPage.js
+++ src/components/GreetingPage.js
@@ -5,8 +5,11 @@ const GreetingPage = props => {
   return (
     <React.Fragment>
       <div className="jumbotron">
-        <h1 className="display-4">Student Pair Randomizer</h1>
-        <p className="lead">The student pair randomizer is the perfect tool for fairly pairing students up for class assignments and projects. You can add classes, students to each class, and configure the minimum distance to the last identical pairing.</p>
+        <h1 className="display-4">Project Group Randomizer</h1>
+        <p className="lead">In courses where students are frequently paired for lab projects, optimal pairing of students can be a challenge.</p>
+        <p className="lead">Of the many approaches, randomized pairing has measurable advantages, but can be challenging administer consistently.</p>
+        <p className="lead">The Randomizer helps solve that problem, evenly distributing students among pairs, ensuring they haven't happened less than a pre-fixed distance ago, and tracking those pairings.</p>
+        <p className="lead">Create an account to start pairing your students.</p>
         <hr className="my-4" />
         <p className="lead">
           <Link className="btn btn-primary" to="/sign-up" role="button">Sign Up</Link>
